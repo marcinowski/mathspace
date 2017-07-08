84b4f4475be9fe7f97c7385c3ee3b3a14df74ff8
Marcin <muszynskimarcin@wp.pl>
2017-07-08 12:47:30 +0200

    Styling of template

--- simple.py
+++ simple.py
@@ -9,9 +9,23 @@ from flask import Flask, render_template, request, url_for
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
+    """ Base routing app for main page view """
     url_for('static', filename='bootstrap.min.css')
     if request.method == 'POST':
-        pass
+        try:
+            name = request.form.get('city', '')
+            number = ConvertCityName().convert(name)
+            return render_template('simple_main.html', results=True, number=number)
+        except Exception as e:
+            return render_template('simple_main.html', error=True, reason=e)
     return render_template('simple_main.html')
+
+
+class ConvertCityName(object):
+    def __init__(self):
+        pass
+
+    def convert(self, name):
+        return name
