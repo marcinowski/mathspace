2b90f9f94eaf39a23055faeeb0efc3be9c2a791d
Marcin <muszynskimarcin@wp.pl>
2017-07-08 13:52:40 +0200

    Logical tests

--- test.py
+++ test.py
@@ -9,21 +9,38 @@ from unittest import TestCase
 from simple import ConvertCityName, WrongNameFormat
 
 
-class TestConvertCityName(TestCase):
+class TestConvertWrongCityName(TestCase):
     def test_capital_error_handling(self):
+        """ Should fail on capitals - accepts only lower case """
         with self.assertRaises(WrongNameFormat):
-            ConvertCityName('TeSt')
+            ConvertCityName('teSt')
 
     def test_empty_error_handling(self):
+        """ Should fail on empty values """
         with self.assertRaises(WrongNameFormat):
             ConvertCityName('')
         with self.assertRaises(WrongNameFormat):
             ConvertCityName(None)
 
     def test_number_error_handling(self):
+        """ Should fail on non string input """
         with self.assertRaises(WrongNameFormat):
             ConvertCityName(5)
-#
-# def test_converting(self):
-#     c = ConvertCityName('')
-    
+
+
+class TestConvertCityName(TestCase):
+    def test_general_concept(self):
+        """ Simple test for conceptual purpose """
+        c = ConvertCityName('def').convert()
+        result = str(ord('d')) + str(ord('e')) + str(ord('f'))  # 100101102
+        self.assertEqual(c, result)
+
+    def test_first_letter_convertion(self):
+        """ First letter should be converted to negative if less than 3 digits """
+        c = ConvertCityName('ad').convert()
+        self.assertEqual(c, '-97100')
+
+    def test_abc_in_string(self):
+        """ a, b & c as 2 digit numbers should be followed by 0 """
+        c = ConvertCityName('eaebece').convert()
+        self.assertEqual(c, '101097101098101099101')
